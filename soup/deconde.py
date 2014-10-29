5c74b6a3f15a48f377bd3dc3174506870d80dd54
klob <185902156@qq.com>
2014-10-29 22:44:01 +0800

    1.1

--- soup/deconde.py
+++ soup/deconde.py
@@ -9,6 +9,8 @@ import sys
 from bs4 import BeautifulSoup
 from markdown import markdown
 
+from soup.mark import Html2MarkdownParser
+
 
 def mkdir(filedir):
     if (not (os.path.exists(filedir))):
@@ -20,10 +22,7 @@ def getasay(url):
     soup = BeautifulSoup(response)
     tmp = getPageURLs(url)
     for k in tmp:
-        try:
-            saveart(soup.title.string, 'http://www.jianshu.com' + k)
-        except:
-            print('error')
+        saveart(soup.title.string, 'http://www.jianshu.com' + k)
 
 
 def getPageURLs(url):
@@ -39,32 +38,39 @@ def saveart(dir, url):
     soup = BeautifulSoup(response)
     mkdir(dir)
     content = soup.findAll(attrs={"class": "show-content"})
-    title = soup.findAll(attrs={"class": "article"})
+    arc = content[0]
+    title = soup.findAll('title')
     filepath = os.getcwd() + os.sep + dir + os.sep + soup.title.string
-    print(url)
-    print filepath
     f = open(r'' + filepath + '.htm', 'w')
-    cont = str(title[0])
-    f.write(cont)
+    cont = str(content[0])
+    f.writelines(cont)
     f.close()
+
     input_file = codecs.open(filepath + '.htm', mode="r", encoding="utf8")
     text = input_file.read()
     html = markdown(text)
-    # print
-    print html
-    # Write string html to disk
+
     output_file = codecs.open(filepath + '.html', mode="w", encoding="utf8")
     output_file.write(html)
     output_file.close()
 
+    p = Html2MarkdownParser()
+    p.feed(text)
+    print(text)
+    output_file = codecs.open(filepath + '.markdown', mode="w", encoding="utf8")
+    output_file.write("# " + soup.title.text + p.get_markdown())
+    print(p.get_markdown())
+    p.close()
+    # print p.get_markdown()
 
 
 def start():
     thousand = 'http://www.jianshu.com/collection/f16b3d483ec2?max_id='
-    a = range(16, 64, 1)
-    for i in a:
-        print(i)
-        getasay(thousand + str(i * 900))
+    a = range(16, 17, 1)
+    # for i in a:
+    # print(i)
+    # getasay(thousand + str(i * 900))
+    saveart('mark', 'http://www.jianshu.com/p/6d42ad6368fd')
 
 
 if __name__ == '__main__':
